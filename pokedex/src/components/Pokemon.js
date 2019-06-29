8613a3357c278394185f0899523bc3524642a74f
Ashwani Goswami <goswami17829@gmail.com>
2019-06-29 12:18:57 +0530

    add search functionality

--- pokedex/src/components/Pokemon.js
+++ pokedex/src/components/Pokemon.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 class Pokemon extends Component {
     state = {
-        pokemons: []
+        pokemons: [],
+        searchedPoke: []
     }
     componentDidMount = () => {
         fetch('https://pokeapi.co/api/v2/pokemon/')
@@ -12,17 +13,43 @@ class Pokemon extends Component {
             pokemons: data.results
         }))
     }
+    handleChange = (e) => {
+        const searchedVal = e.target.value;
+        this.setState({
+          ...this.state,
+          searchedPoke: this.state.pokemons.filter(item => item.name.includes(searchedVal))  
+        })
+        console.log(this.state.searchedPoke)
+    }
     render() {
-        const { pokemons } = this.state;
+        const { pokemons, searchedPoke } = this.state;
         return (
-            <div className="poke-wrapper">
+            <div>
+                <form className="search-form">
+                    <input type="text" placeholder="Search for Pokemon" className="search-input" onChange={this.handleChange} />
+                </form>
                 {
-                    pokemons && pokemons.map((p,i) => 
+            searchedPoke.length ? 
+                <div className="poke-wrapper">
+                    {
+                        searchedPoke.map((p,i) => 
                         <Link to={`pokemon/${i+1}`} className="poke-box">
                             <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i+1}.png`} />
                             <p>{p.name}</p>
                         </Link>
                     )
+                    }
+                </div> :
+                <div className="poke-wrapper">
+                    {
+                        pokemons && pokemons.map((p,i) => 
+                            <Link to={`pokemon/${i+1}`} className="poke-box">
+                                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i+1}.png`} />
+                                <p>{p.name}</p>
+                            </Link>
+                        )
+                    }
+                </div>
                 }
             </div>
         );
